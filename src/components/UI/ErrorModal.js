22097b4df6d86b1a427cfaa02133cdac5e08eaa0
Duy Pham <duypham9895@gmail.com>
2022-09-07 12:37:12 +0700

    Implement ReactDOM.createPortal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,25 +1,50 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import classes from "./ErrorModal.module.css";
 import Button from "./Button";
 import Card from "./Card";
 
+// These two containers are siblings in the DOM
+const backdropRoot = document.getElementById("backdrop-root");
+const overlayRoot = document.getElementById("overlay-root");
+
+const Backdrop = ({ onCancelModal }) => {
+  return <div className={classes.backdrop} onClick={onCancelModal} />;
+};
+
+const ModalOverlay = ({ title, message, onCancelModal }) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onCancelModal}>Cancel</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, message, onCancelModal }) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={onCancelModal} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={onCancelModal}>Cancel</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onCancelModal={onCancelModal} />,
+        backdropRoot
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          message={message}
+          onCancelModal={onCancelModal}
+        />,
+        overlayRoot
+      )}
+    </>
   );
 };
 
